afcd6da1df116b39aaa4f1718de600de1c617ba2
Kgosi Motlogelwa <komotlogelwa@gmail.com>
2023-02-01 16:34:59 +0200

    removed secret key

--- firstProj/firstProj/settings.py
+++ firstProj/firstProj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--4v2tynw+b7aoe1+(_lm5pioo)ln+at3=pe=nn*(7wy#)89y6)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
